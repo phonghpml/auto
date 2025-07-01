a907c87d656f1772c8d025bb54172d4c4b88a08c
Phong Bùi <buitrongphonghp@gmail.com>
2025-07-01 22:28:16 +0700

    update code

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
-
+import puppeteer from 'puppeteer';
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async runAuto() {
+    // Or import puppeteer from 'puppeteer-core';
+
+    // Launch the browser and open a new blank page
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
   }
 }
